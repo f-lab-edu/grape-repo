9e2a688b55c17c8e5bc98e62752c699c402d55ae
xxziiko <xxziiko@gmail.com>
2024-08-08 01:14:40 +0900

    chore: 환경변수 설정

--- src/apps/routes/__root.tsx
+++ src/apps/routes/__root.tsx
@@ -6,7 +6,7 @@ export const Route = createRootRoute({
   component: () => (
     <AuthProvider>
       <Outlet />
-      <TanStackRouterDevtools />
+      {import.meta.env.MODE === 'development' && <TanStackRouterDevtools />}
     </AuthProvider>
   ),
 });
