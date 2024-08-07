bcd866f7227dad98fb30ab045c8ae1a6a2314617
xxziiko <xxziiko@gmail.com>
2024-08-07 21:48:14 +0900

    chore: 빌드파일 수정

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-cd ../
-mkdir output
-cp -R ./grape-repo/* ./output
-cp -R ./output ./grape-repo/
+set -e  # 스크립트 실행 중 오류 발생 시 중단
+
+# 현재 위치를 기준으로 경로 설정
+OUTPUT_DIR=$(pwd)/output
+GRAPE_REPO_DIR=$(pwd)/grape-repo
+
+# output 디렉토리 생성
+mkdir -p $OUTPUT_DIR
+
+# grape-repo 디렉토리의 내용을 output 디렉토리로 복사
+cp -R $GRAPE_REPO_DIR/* $OUTPUT_DIR
