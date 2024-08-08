f7d65985e2cb390cf9d0d712d78607c6f677297a
xxziiko <xxziiko@gmail.com>
2024-08-08 15:18:15 +0900

    chore: 충돌 해결

--- src/apps/routes/__root.tsx
+++ src/apps/routes/__root.tsx
@@ -1,12 +1,11 @@
-import { AuthProvider } from '@/entities/auth';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
-    <AuthProvider>
+    <>
       <Outlet />
       {import.meta.env.MODE === 'development' && <TanStackRouterDevtools />}
-    </AuthProvider>
+    </>
   ),
 });
