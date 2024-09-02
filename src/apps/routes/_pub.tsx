ed71650c69ad4a26da1e183bea2de43abe9a0b4a
xxziiko <xxziiko@gmail.com>
2024-09-03 03:24:17 +0900

    refactor: arrow function 제거

--- src/apps/routes/_pub.tsx
+++ src/apps/routes/_pub.tsx
@@ -2,8 +2,6 @@ import { signOutUser } from '@/entities/auth';
 import { Outlet, createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_pub')({
-  beforeLoad: async () => {
-    signOutUser();
-  },
+  beforeLoad: signOutUser,
   component: () => <Outlet />,
 });
