8c08ea2ec9c8c00b6474f5f762fdf54e497c46c5
xxziiko <xxziiko@gmail.com>
2024-09-23 22:38:52 +0900

    refactor: fetch 함수명, 구조 변경

--- src/features/loginForm/api/checkUserNameExists.tsx
+++
@@ -1,6 +0,0 @@
-import { handleError, selectEmail } from '@/shared';
-
-const checkUserNameExists = async (email: string | undefined) =>
-  selectEmail(email).then(handleError);
-
-export default checkUserNameExists;
--- src/features/loginForm/index.ts
+++ src/features/loginForm/index.ts
@@ -6,4 +6,4 @@ export { default as useLoginMutation } from "./model/useLoginMutation";
 export { default as useSignUpMutation } from "./model/useSignUpMutation";
 
 // apis
-export { default as checkUserNameExists } from "./api/checkUserNameExists";
+export { default as fetchUserName } from "./api/fetchUserName";
